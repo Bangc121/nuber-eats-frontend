dfc7446cd108363b28c035b9df930b3974ae407c
Bangc121 <wjdrlalf17@gmail.com>
2021-07-07 00:57:56 +0900

    form login

--- src/pages/create-account.tsx
+++ src/pages/create-account.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
+import React from "react";
 
 export const CreateAccount = () => {
-    return <span>Create Account</span>;
-}
+  return <span>Create Account</span>;
+};
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -38,22 +38,24 @@
 //         </div>
 //     )
 // }
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { CreateAccount } from '../pages/create-account';
-import { Login } from '../pages/login';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CreateAccount } from "../pages/create-account";
+import { Login } from "../pages/login";
 
 export const LoggedOutRouter = () => {
-    return <Router>
-        <Switch>
-            <Route path="/create-account">
-                <CreateAccount />
-            </Route>
-            <Route path="/">
-                <Login />
-            </Route>
-        </Switch>
-    </Router>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/create-account">
+          <CreateAccount />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 //types이 뭘까
